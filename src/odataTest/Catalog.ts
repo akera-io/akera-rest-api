4dd6007698b24ba5e98677a5700071dc46220f50
Porumb Bogdan <porumbbogdan1999@gmail.com>
2020-07-30 12:50:27 +0300

    Refactor catalog

--- src/odataTest/Catalog.ts
+++ src/odataTest/Catalog.ts
@@ -1,44 +1,16 @@
-import { Direction, DataType } from "@akeraio/api";
+import { Direction, DataType, IABLCall , IDatasetSchema, ITableSchema} from "@akeraio/api";
 
 import { Edm } from "odata-v4-metadata";
-import { Convert } from "odata-v4-server";
+
 
 interface AkeraRestCatalog {
   namespace?: string;
-  functions?: AkeraRestFunction[];
-  actions?: AkeraRestAction[];
+  functions?: API[];
+ 
 }
 
-interface AkeraRestFunction extends AkeraRestAction {
-  return: DataType | TempTable | DataSet;
+interface API extends IABLCall {
+  schema:ITableSchema| IDatasetSchema;
 }
 
-interface AkeraRestAction {
-  object: string;
-  method?: string;
-  parameters?: Parameter[];
-}
 
-interface Parameter {
-
-  mode: Direction;
-  dataType: DataType;
-  dataStructure?: TempTable | DataSet;
-}
-
-interface TempTable {
-  name: string;
-  fields: Field[];
-}
-
-interface DataSet {
-  name: string;
-  tables: TempTable[];
-}
-
-interface Field {
-  name: string;
-  dataType: DataType;
-}
-
-Edm.Schema
