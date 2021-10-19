160666b0a51ee6326eb4f4989b9bd9618cae023b
Marian Edu <marian.edu@acorn.ro>
2021-10-19 11:36:46 +0300

    extend catalog

--- src/odataTest/Catalog.ts
+++ src/odataTest/Catalog.ts
@@ -2,29 +2,47 @@ import {
   IABLCall,
   IDatasetSchema,
   ITableSchema,
+  IParameter,
+  Direction,
+  DataType
 } from "@akeraio/api";
-import {Edm} from "odata-v4-metadata";
+import { Edm } from "odata-v4-metadata";
 
 
 // let catalog:AkeraRestCatalog[];
 // let schema:Edm.Schema;
 interface AkeraRestCatalog {
   namespace?: string;
-  functions?:Array<API>;
-}
-interface API extends IABLCall {
-
-  schema: [ITableSchema | IDatasetSchema];
+  functions?: Array<IAPI>;
 }
 
-let convert=function(catalog:AkeraRestCatalog){
-  let schema:Edm.Schema;
-  let funcSchema:Edm.Function;
-  schema.namespace=catalog.namespace;
-   function function1() {
-    
+interface IAPIParameter extends IParameter {
+  name: string;
+  complexType?: Object;
+}
+
+interface IAPI extends IABLCall {
+  name: string;
+  schema?: [ITableSchema | IDatasetSchema];
+  parameters?: IAPIParameter[];
+}
+
+class APIParameter implements IAPIParameter {
+  direction: Direction;
+  type?: DataType;
+  schema?: ITableSchema | IDatasetSchema;
+  name: string;
+
+}
+
+let convert = function (catalog: AkeraRestCatalog) {
+  let schema: Edm.Schema;
+  let funcSchema: Edm.Function;
+  schema.namespace = catalog.namespace;
+  function function1() {
+
   }
-  
+
 }
 
 
--- src/odataTest/test.ts
+++ src/odataTest/test.ts
@@ -1,4 +1,4 @@
-import {ODataServer, odata, Edm, ODataController} from "odata-v4-server";
+import { ODataServer, odata, Edm, ODataController } from "odata-v4-server";
 import * as express from "express";
 import { stringWriter } from "xmlbuilder";
 
@@ -11,6 +11,14 @@ class findProduct_Product {
 
   @Edm.Int32
   age?: number;
+}
+
+class getProduct_Product {
+  @Edm.String
+  Name?: string;
+
+  @Edm.Collection(Edm.ComplexType(findProduct_Product))
+  products?: findProduct_Product[];
 }
 
 // @odata.type(Product)
@@ -26,13 +34,36 @@ export class AkeraApiServer extends ODataServer {
   @Edm.FunctionImport
   @Edm.ComplexType(findProduct_Product)
   findProduct(@Edm.Int64 num: number) {
-   let ctype: findProduct_Product;
-   ctype.Name="Andrei";
-   ctype.age=23;
+    let ctype: findProduct_Product;
+    ctype.Name = "Andrei";
+    ctype.age = 23;
     return ctype
 
   }
 
+
+  @Edm.FunctionImport
+  @Edm.ComplexType(getProduct_Product)
+  getProducts(@Edm.Int64 num: number) {
+    let ctype: getProduct_Product = {Name: "Andrei"};
+
+    ctype.products = [{ Name: "test", age: 23 },
+    { Name: "test2", age: 123 }];
+    return ctype
+
+  }
+
+  @Edm.ActionImport()
+  @Edm.ComplexType(findProduct_Product)
+  setProduct(
+    @odata.body
+    @Edm.ComplexType(findProduct_Product) prod: findProduct_Product) {
+    prod.age = prod.age ? prod.age + 10 : 99;
+
+    console.log(prod);
+    return prod;
+
+  }
 
   //static $metadata (): ServiceMetadata {
   // return ServiceMetadata.processMetadataJson(schema);
